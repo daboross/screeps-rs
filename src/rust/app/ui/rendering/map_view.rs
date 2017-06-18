8aa7d1feef036914f65a6deb7b1ff39755ff1337
David Ross <daboross@daboross.net>
2017-06-18 01:59:53 -0700

    Fix rendering so rooms aren't upside down

--- src/rust/app/ui/rendering/map_view.rs
+++ src/rust/app/ui/rendering/map_view.rs
@@ -258,7 +258,7 @@ impl<'a> Iterator for ConstructedViewIterator<'a> {
                                     (*current_relative_room_x as f64 + ((*current_terrain_x as f64) / 50.0));
                         let y_pos = self.start_room_screen_pos.1 +
                                     self.room_screen_size *
-                                    (*current_relative_room_y as f64 + ((*current_terrain_y as f64) / 50.0));
+                                    (*current_relative_room_y as f64 + (((50 - *current_terrain_y) as f64) / 50.0));
 
                         Primitive {
                             id: self.render_id,
@@ -343,7 +343,8 @@ impl<'a> Iterator for ConstructedViewIterator<'a> {
                                     room_screen_size * (current_relative_room_x as f64 + ((in_room_x as f64) / 50.0)) +
                                     terrain_square_length;
                         let y_pos = start_room_screen_pos.1 +
-                                    room_screen_size * (current_relative_room_y as f64 + ((in_room_y as f64) / 50.0)) +
+                                    room_screen_size *
+                                    (current_relative_room_y as f64 + (((50 - in_room_y) as f64) / 50.0)) +
                                     terrain_square_length;
 
                         let visual_length = terrain_square_length;
