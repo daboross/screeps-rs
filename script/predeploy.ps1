c11bb8f2d4353bc55015db72eba6c2c473a2728a
David Ross <daboross@daboross.net>
2017-06-19 01:06:54 -0700

    Add deploy logic to travis and appveyor configs.

---
+++ script/predeploy.ps1
@@ -0,0 +1,19 @@
+$SRC_DIR = $PWD.Path
+$STAGE = [System.Guid]::NewGuid().ToString()
+
+Set-Location $ENV:Temp
+New-Item -Type Directory -Name $STAGE
+Set-Location $STAGE
+
+$ZIP = "$SRC_DIR\$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET_DESC).zip"
+
+Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\scrs.exe" '.\'
+
+7z a "$ZIP" *
+
+Push-AppveyorArtifact "$ZIP"
+
+Remove-Item *.* -Force
+Set-Location ..
+Remove-Item $STAGE
+Set-Location $SRC_DIR
