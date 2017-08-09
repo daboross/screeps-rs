ca342c560610bcaf8d0241c6be1c8751c961b467
David Ross <daboross@daboross.net>
2017-08-09 00:52:16 -0700

    Remove scrs-jvm project.

--- scrs-jvm/src/jni_c_header.rs
+++
@@ -1,3 +0,0 @@
-//! Generated module for Java interaction.
-#![allow(non_upper_case_globals, dead_code, non_camel_case_types, improper_ctypes, non_snake_case)]
-include!(concat!(env!("OUT_DIR"), "/jni_c_header.rs"));
--- scrs-jvm/src/lib.rs
+++
@@ -1,4 +0,0 @@
-// Shim for `src/main/rust/lib.rs`
-include!(concat!(env!("OUT_DIR"), "/lib.rs"));
-
-mod jni_c_header;
